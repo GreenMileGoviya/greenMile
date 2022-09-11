903f93a3bdc34d651e6818bbd541c15eda2210b4
Kumudperera <nethmalperera54@gmail.com>
2022-09-11 17:31:43 +0530

    Image adding modified

--- ecoAgriFrontEnd/src/requestMethods.js
+++ ecoAgriFrontEnd/src/requestMethods.js
@@ -2,12 +2,12 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 
 const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyY2JlMjE5NzQ3YTI4NWM0ZTJmMzFiYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1Nzc0NDY5OCwiZXhwIjoxNjU4MDAzODk4fQ.7daxD3CSubuHEJPl-IMtp_lguXimAIULd46BbJ-I8PQ";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyY2JlMjE5NzQ3YTI4NWM0ZTJmMzFiYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1Nzc0NDY5OCwiZXhwIjoxNjU4MDAzODk4fQ.7daxD3CSubuHEJPl-IMtp_lguXimAIULd46BbJ-I8PQ";
 
 // const user = useSelector((state) => state.user.token);
-const TOKEN = JSON.parse(
-  JSON.parse(localStorage.getItem("persist:root")).user
-).token;
+// const TOKEN = JSON.parse(
+//   JSON.parse(localStorage.getItem("persist:root")).user
+// ).token;
 // const TOKEN = JSON.parse(localStorage.getItem("accessToken"))
 // console.log(TOKEN);
 
